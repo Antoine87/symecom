e8b9e70b113c0c53b4ebebe6d33d81acb3308df9
Stéphane Emploi <moi@moi.com>
2016-12-19 10:52:20 +0100

    Crud sur Book

---
+++ src/ModelBundle/Form/BookType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('price')->add('datePublished')->add('abstract')->add('publisher')->add('author')->add('tags')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ModelBundle\Entity\Book'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'modelbundle_book';
+    }
+
+
+}
