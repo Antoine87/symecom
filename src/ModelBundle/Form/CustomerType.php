44d78fd1a71d4fada7372bc7442bfc8a5f7aa912
Stéphane Emploi <moi@moi.com>
2016-12-20 11:56:26 +0100

    Inscription de l'utilisateur

---
+++ src/ModelBundle/Form/CustomerType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('firstName', TextType::class, ['label' => 'Prénom'])
+            ->add('email', EmailType::class, ['label' => 'adresse e-mail'])
+            ->add('birthDate', DateType::class, [
+                'label' => 'date de naissance', 'widget' => 'single_text'
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ModelBundle\Entity\Customer'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'modelbundle_customer';
+    }
+
+
+}
